d952bdbce93b9d927fd970077a8bdff46765077e
Jefferson França <jefferson.vieiraf@hotmail.com>
2022-10-20 11:15:15 -0300

    feat: set weather info on local storage

---
+++ src/modules/weatherInfo.js
@@ -0,0 +1,45 @@
+const weatherInfo = (() => {
+  async function getLocationWeather(location){
+    try{
+      const weather = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=507218e34ab44f761e2f3ca63a9eaeec`);
+
+      const fetchResult = await weather.json();
+
+      return fetchResult;
+    }
+    catch(err){
+      alert(err);
+    }
+  }
+
+  async function setWeatherInformation(info){
+    localStorage.setItem('place', info.name);
+
+    for(let key in info.main){
+      const value = info.main[`${key}`];
+      localStorage.setItem(key, value);
+    }
+
+    localStorage.setItem('country', info.sys.country);
+    localStorage.setItem('description', info.weather[0].main);
+    localStorage.setItem('icon', info.weather[0].icon);
+    localStorage.setItem('wind', info.wind.speed);
+  }
+  
+  async function reportWeather(location){
+    const report = await getLocationWeather(location);
+
+    try{
+      await setWeatherInformation(report);
+    }
+    catch(err){
+      alert(err);
+    }
+
+    return report;
+  }
+
+  return {reportWeather};
+})();
+
+export default weatherInfo;
